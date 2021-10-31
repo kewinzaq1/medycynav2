17ef4c0f3882c39fe212cb8b9f418aa1d2e44888
kewinzaq1 <77052270+kewinzaq1@users.noreply.github.com>
2021-10-31 06:52:59 +0100

    Add files via upload

---
+++ scroll_reval.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal(".headline");
+ScrollReveal().reveal(".title", { delay: 500, scale: 0.5 });
+ScrollReveal().reveal(".desc", { delay: 800, scale: 0.4 });
+ScrollReveal().reveal(".service-box", {
+  delay: 1200,
+  origin: "bottom",
+  distance: "500px",
+});
+ScrollReveal().reveal(".pl-box", {
+  delay: 1200,
+  origin: "top",
+  distance: "500px",
+});
+ScrollReveal().reveal(".testimonails", { delay: 1200, scale: 0.3 });
+ScrollReveal().reveal(".qanda", { delay: 2200, scale: 0.3 });
+ScrollReveal().reveal(".me", { delay: 800, scale: 0.3 });
